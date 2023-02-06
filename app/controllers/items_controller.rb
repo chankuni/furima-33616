e0776e273528885c7578fbc0a306ec6660720ec5
chankuni <93761060+chankuni@users.noreply.github.com>
2023-02-06 22:10:24 +0900

    商品出品機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,33 @@
 class ItemsController < ApplicationController
 
+  before_action :move_to_index, only: [:new]
+
   def index
   end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.create(item_params)
+    if @item.save
+      redirect_to action: :index
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :description, :category_id, :status_id, :charge_id, :prefecture_id, :duration_id, :price, :image).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to "/users/sign_in"
+    end
+  end
+  
 end
