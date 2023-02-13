1023b30cb405b90ebea64865bc318d3bb454736e
chankuni <93761060+chankuni@users.noreply.github.com>
2023-02-13 20:10:50 +0900

    商品削除機能２

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -28,8 +28,11 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    @item.destroy
-    redirect_to root_path
+    if current_user.id == @item.user_id
+      @item.destroy
+    else
+      redirect_to root_path
+    end
   end
 
   def update
