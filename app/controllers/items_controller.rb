ccc19b14d52a172d73b837d4a3ab855beb7d5835
chankuni <93761060+chankuni@users.noreply.github.com>
2023-02-13 21:17:02 +0900

    商品削除機能３

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit, :destroy]
   before_action :set_item, only: [:show, :edit, :update, :destroy]
-  before_action :move_to_index, only: [:edit]
+  before_action :move_to_index, only: [:edit, :destroy]
 
 
   def index
@@ -28,11 +28,8 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    if current_user.id == @item.user_id
       @item.destroy
-    else
       redirect_to root_path
-    end
   end
 
   def update
